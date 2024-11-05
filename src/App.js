186e1fbf7f33886fa201a3a3c8176262e7feccc1
Yohannes G.Simegne <ygetahun37@gmail.com>
2024-11-05 13:21:20 +0300

    Initial styling done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-function App() {
-  return <div>My App</div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌊🌴 Far away 👜</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">List</div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜You have X items on your list, and you already packed X (X%) </em>
+    </footer>
+  );
+}
